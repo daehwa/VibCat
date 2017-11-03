f14052651d2b0a0aec77ede2b021138a26510357
Daehwa <eoghk7323@naver.com>
2017-11-03 12:55:22 +0900

    save data

--- app/src/main/java/com/example/eoghk/vibcat/MainActivity.java
+++ app/src/main/java/com/example/eoghk/vibcat/MainActivity.java
@@ -7,10 +7,17 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
@@ -19,13 +26,16 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     TextView textView;
     Vibrator vide;
     boolean isVibrating;
-    String result;
+    String result, learning_data;
+    EditText et;
+    int index = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView = (TextView)findViewById(R.id.text);
+        et = (EditText)findViewById(R.id.test_num);
 
         sm = (SensorManager)getSystemService(SENSOR_SERVICE);
         accSensor = sm.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
@@ -42,8 +52,11 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         float z = event.values[2]*50;
         String str = "x: "+x+"\n"+"y: "+y+"\n"+"z: "+z;
         textView.setText(str);
-        if (isVibrating)
-            result +=z+"\n";
+        if (isVibrating) {
+            result += z + "\n";
+            learning_data += index+":"+z+" ";
+            index++;
+        }
     }
 
     @Override
@@ -58,6 +71,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             public void run()
             {
                 result = "";
+                learning_data = et.getText().toString() +" ";
                 isVibrating = true;
 
                 try {
@@ -67,6 +81,22 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                 }
 
                 isVibrating = false;
+                index = 0;
+
+                //write in SD card
+                String state= Environment.getExternalStorageState();
+                File path= Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+                File  file= new File(path, "VibCatData.txt");
+
+                try {
+                    FileWriter wr= new FileWriter(file,true); //두번째 파라미터 true: 기존파일에 추가할지 여부를 나타냅니다.
+                    PrintWriter writer= new PrintWriter(wr);
+                    writer.println(learning_data+"\n");
+                    writer.close();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
             }
         }).start();
     }
