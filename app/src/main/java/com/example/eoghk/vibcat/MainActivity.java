10e492aa5ffb66faa99e27b436cb02813a621d19
Daehwa <eoghk7323@naver.com>
2017-11-03 21:57:01 +0900

    integrate SVM model

--- app/src/main/java/com/example/eoghk/vibcat/MainActivity.java
+++ app/src/main/java/com/example/eoghk/vibcat/MainActivity.java
@@ -6,32 +6,45 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
 import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+
+import umich.cse.yctung.androidlibsvm.LibSVM;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     SensorManager sm;
     Sensor accSensor;
     TextView textView;
+    TextView result_tv;
     Vibrator vide;
     boolean isVibrating;
     String result, learning_data;
     EditText et;
     int index = 0;
     float []  arr = new float [96];
+    String systemPath = Environment.getExternalStorageDirectory() + "/";
+    private Handler mHandler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +52,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         setContentView(R.layout.activity_main);
         textView = (TextView)findViewById(R.id.text);
         et = (EditText)findViewById(R.id.test_num);
+        result_tv = (TextView)findViewById(R.id.result);
 
         sm = (SensorManager)getSystemService(SENSOR_SERVICE);
         accSensor = sm.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
@@ -117,6 +131,69 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             }
         }).start();
     }
+    public void TestIt(View v){
+        vide.vibrate(1000);
+        new Thread(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                result = "";
+                learning_data = "0";
+                isVibrating = true;
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                isVibrating = false;
+                index = 0;
+
+                //write in SD card
+                String SDCARD_FILE_PATH = "/sdcard/study/";
+                String filename = "test_file";
+                Arrays.sort(arr);
+
+                for (int i = 0; i<96;i++){
+                    learning_data += " "+ i + ":" + arr[i];
+                }
+
+                try {
+                    FileWriter fw = new FileWriter(SDCARD_FILE_PATH + filename,false);
+                    PrintWriter writer=new PrintWriter(fw, false);
+                    writer.write(learning_data+"\n");
+                    writer.close();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                List<String> commands = new ArrayList<>();
+                commands.add("-b");
+                commands.add(Integer.toString(0));
+                String testFilePicker = systemPath+"study/test_file";
+                commands.add(testFilePicker);
+                String modelFilePicker = systemPath+"study/VibCat.model";
+                commands.add(modelFilePicker);
+                commands.add(systemPath+"study/result_file");
+                new getResult().execute(commands.toArray(new String[0]));
+
+                mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        // This gets executed on the UI thread so it can safely modify Views
+                        result_tv.setText(objClassifier());
+                    }
+                });
+
+            }
+        }).start();
+    }
 
     public void Send(View v){
         Intent intent = new Intent(android.content.Intent.ACTION_SEND);
@@ -127,5 +204,43 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 // Title of intent
         Intent chooser = Intent.createChooser(intent, "친구에게 공유하기");
         startActivity(chooser);
+    }
+
+    private class getResult extends AsyncTask<String, Void, Void> {
+        @Override
+        protected Void doInBackground(String... params) {
+            LibSVM.getInstance().predict(TextUtils.join(" ", params));
+            return null;
+        }
+    }
+
+    private  String objClassifier(){
+        String r = "";
+        File file = new File(systemPath+"study/result_file");
+        FileReader fr = null ;
+
+        int value=0;
+
+        try {
+            fr = new FileReader(file) ;
+            value = fr.read()-48;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("hello: "+value);
+        switch(value){
+            case 0:
+                r = "Desk";
+                break;
+            case 1:
+                r = "Hand";
+                break;
+            default:
+                r = "Not Classified";
+        }
+
+        return r;
     }
 }
